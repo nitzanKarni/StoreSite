25bf044da99c00789f981486a0b7979a7badf81f
nitzanKarni <narnid15@gmail.com>
2017-02-15 20:00:12 +0200

    jquery aadded and card added

--- public/javascripts/index-controller.js
+++ public/javascripts/index-controller.js
@@ -1,5 +1,17 @@
-//var app = angular.module('mainApp',['ngRoute','ngMaterial','ngAria','ngAnimate'])
+var app = angular.module('mainApp',[]);
 
-//app.controller('indexController',['$scope', '$http', function($scope, $http) {
- //   $scope.test = "test1212";
-//}]);
+app.controller('index',['$scope', '$http', function($scope, $http) {
+    
+    // Simple GET request example:
+    $http({
+        method: 'GET',
+        url: '/127.0.0.1:9000/products'
+    }).then(function successCallback(response) {
+        $scope.products = JSON.parse(response.data);
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+    
+    $scope.imagePath = 'images/530x500_back_1.jpg';
+}]);
