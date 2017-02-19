bdccc079942a8a1f7767c90131c95c31eaeee946
nitzanKarni <narnid15@gmail.com>
2017-02-19 19:37:49 +0200

    Creation of html prototipe

--- public/javascripts/index-controller.js
+++ public/javascripts/index-controller.js
@@ -1,17 +1,18 @@
-var app = angular.module('mainApp',[]);
+var app = angular.module('mainApp',['ngMaterial']);
 
 app.controller('index',['$scope', '$http', function($scope, $http) {
     
     // Simple GET request example:
-    $http({
-        method: 'GET',
-        url: '/127.0.0.1:9000/products'
-    }).then(function successCallback(response) {
-        $scope.products = JSON.parse(response.data);
-    }, function errorCallback(response) {
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-    });
+   $http({
+       method: 'GET',
+       url: '/127.0.0.1:9000/products'
+   }).then(function successCallback(response) {
+       $scope.products = JSON.parse(response.data);
+   }, function errorCallback(response) {
+       // called asynchronously if an error occurs
+       // or server returns response with an error status.
+   });
     
     $scope.imagePath = 'images/530x500_back_1.jpg';
+    $scope.title1 = 'Good morning';
 }]);
