2bb47d7afe0f363457854d1bc50363b5e4d5ff5d
nitzanKarni <narnid15@gmail.com>
2017-02-26 19:36:55 +0200

    route added

--- public/javascripts/index-controller.js
+++ public/javascripts/index-controller.js
@@ -1,6 +1,6 @@
-var app = angular.module('mainApp',['ngMaterial']);
+var app = angular.module('mainApp',['ngMaterial','ngRoute']);
 
-app.controller('index',['$scope', '$http', function($scope, $http) {
+app.controller('index',['$scope', '$http', function($scope, $http, $location) {
     
     // Simple GET request example:
    $http({
@@ -15,4 +15,32 @@ app.controller('index',['$scope', '$http', function($scope, $http) {
     
     $scope.imagePath = 'images/530x500_back_1.jpg';
     $scope.title1 = 'Good morning';
+
+
+    $scope.selectedIndex = 0;
+
+        $scope.$watch('selectedIndex', function(current, old) {
+            switch (current) {
+                case 0:
+                    $location.url("/view1");
+                    break;
+                case 1:
+                    $location.url("/view2");
+                    break;
+            }
+        });
 }]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "/views/catalog.html",
+        controller : "/javascripts/index-controller.js",
+        url: "/view1",
+    }).when("/catalog", {
+        templateUrl : ""
+    }).when("/about", {
+        templateUrl : "/views/about.html",
+        url: "/view2"
+    })
+});
